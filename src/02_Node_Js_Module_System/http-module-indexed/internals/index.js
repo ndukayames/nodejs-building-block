168e5963bbe57cf7d40f0eafbff6ea1c9a819589
Obi James Onyemaechi <jamesobi@softprism.org>
2022-10-22 22:13:28 +0100

    node module system

---
+++ src/02_Node_Js_Module_System/http-module-indexed/internals/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const { sendRequest } = require("./request");
+const { getResponse } = require("./response.js");
+
+module.exports = {
+  sendRequest,
+  getResponse,
+};
---
+++ src/02_Node_Js_Module_System/http-module-indexed/internals/response.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+function decryptData(encData) {
+  console.log(`decrypting data ...`);
+  return "data";
+}
+
+function getResponse() {
+  return decryptData(123);
+}
+
+module.exports = {
+  getResponse,
+};
